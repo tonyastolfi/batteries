3d7530926f30423807b19ded3258ce41e1f7a8fe
tony.astolfi <tony.astolfi@tamr.com>
2019-12-15 09:33:06 -0500

    Initial commit.

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, CMake
+
+
+class BatteriesConan(ConanFile):
+    name = "batteries"
+    version = "0.1"
+    license = "Apache Public License 2.0"
+    author = "Tony Astolfi <tastolfi@gmail.com>"
+    url = "https://github.com/tonyastolfi/batteries.git"
+    description = "C++ Essentials left out of std:: and boost::"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
