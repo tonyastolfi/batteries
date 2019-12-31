c57bb25e20a692f1263c677292d0d0f2aa71fc87
Tony Astolfi <tony.astolfi@stresearch.com>
2019-12-31 12:50:43 -0500

    Some changes.

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
-#include "hello.h"
+// A change.
 
-void hello(){
-    #ifdef NDEBUG
-    std::cout << "Hello World Release!" <<std::endl;
-    #else
-    std::cout << "Hello World Debug!" <<std::endl;
-    #endif
+#include "hello.h"
+#include <iostream>
+
+void
+hello()
+{
+#ifdef NDEBUG
+    std::cout << "Hello World Release!" << std::endl;
+#else
+    std::cout << "Hello World Debug!" << std::endl;
+#endif
 }
