e08018bcbe54151ed379464fa49b39c9aa92cd1c
Anthony Astolfi <tastolfi@gmail.com>
2022-02-09 17:26:18 -0500

    Add signed/unsigned conversions to checked_cast.

--- src/batteries/buffer.hpp
+++ src/batteries/buffer.hpp
@@ -1,4 +1,5 @@
-// Copyright 2021 Anthony Paul Astolfi
+//######=###=##=#=#=#=#=#==#==#====#+==#+==============+==+==+==+=+==+=+=+=+=+=+=+
+// Copyright 2021-2022 Anthony Paul Astolfi
 //
 #pragma once
 #ifndef BATTERIES_BUFFER_HPP
--- src/batteries/checked_cast.hpp
+++ src/batteries/checked_cast.hpp
@@ -13,7 +13,8 @@ namespace batt {
 
 // Coerce between integral types, panicking if there is a loss of precision.
 //
-template <typename ToType, typename FromType>
+template <typename ToType, typename FromType,
+          typename = std::enable_if_t<std::is_signed_v<ToType> == std::is_signed_v<FromType>>>
 ToType checked_cast(FromType val)
 {
     static_assert(std::is_integral_v<ToType> && std::is_integral_v<FromType>,
@@ -27,6 +28,48 @@ ToType checked_cast(FromType val)
     }
 
     return static_cast<ToType>(val);
+}
+
+// Go from unsigned to signed.
+//
+template <typename ToType, typename FromType,
+          typename = std::enable_if_t<std::is_signed_v<ToType> && !std::is_signed_v<FromType>>,
+          typename = void>
+ToType checked_cast(FromType val)
+{
+    static_assert(std::is_integral_v<ToType> && std::is_integral_v<FromType>,
+                  "checked_cast can only be used with builtin integer types");
+
+    // If going from an unsigned to a larger signed, just coerce.
+    //
+    if (sizeof(FromType) < sizeof(ToType)) {
+        return static_cast<ToType>(val);
+    }
+
+    static constexpr isize kToBits = sizeof(ToType) * 8;
+    static constexpr FromType kRetainedMask = (FromType{1} << (kToBits - 1)) - 1;
+
+    BATT_CHECK_EQ((val & kRetainedMask), val);
+
+    return static_cast<ToType>(val);
+}
+
+// Go from signed to unsigned
+//
+template <typename ToType, typename FromType,
+          typename = std::enable_if_t<!std::is_signed_v<ToType> && std::is_signed_v<FromType>>,
+          typename = void, typename = void>
+ToType checked_cast(FromType val)
+{
+    static_assert(std::is_integral_v<ToType> && std::is_integral_v<FromType>,
+                  "checked_cast can only be used with builtin integer types");
+
+    // Panic if val is negative; otherwise, coerce to the same-sized unsigned type (which is always safe) and
+    // do an unsigned-to-unsigned checked_cast.
+    //
+    BATT_CHECK_GE(val, 0);
+
+    return checked_cast<ToType>(static_cast<std::make_unsigned_t<FromType>>(val));
 }
 
 }  // namespace batt
