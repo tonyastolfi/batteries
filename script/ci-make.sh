a593b39185cb71f092a7bbe70f91e6dfd51450d1
Anthony Astolfi <tastolfi@gmail.com>
2022-04-14 09:22:04 -0400

    Move ci build bash code into "script/ci-make.sh"

--- script/ci-make.sh
+++ script/ci-make.sh
@@ -4,15 +4,16 @@ set -e
 
 script_dir=$(cd $(dirname $0) && pwd)
 project_dir=$(cd ${script_dir}/.. && pwd)
-local_conan_dir=${project_dir}/
+local_conan_parent_dir=${project_dir}/
+local_conan_dir=${local_conan_parent_dir}/.conan
 default_conan_dir=${HOME}/.conan
 
 # Create a local conan cache dir so we can use GitLab file cache.
 #
 if [ ! -e "${local_conan_dir}" ]; then
-    cp -r "${default_conan_dir}" "${local_conan_dir}"
+    cp -r "${default_conan_dir}" "${local_conan_parent_dir}"
 fi
 
 cd "${project_dir}"
 
-CONAN_USER_HOME="${local_conan_dir}" make BUILD_TYPE=Release "$@"
+CONAN_USER_HOME="${local_conan_parent_dir}" make BUILD_TYPE=Release "$@"
