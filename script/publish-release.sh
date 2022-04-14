58ce9daf06b760ccb27e6bbeb56a6f6bb009e6c3
Anthony Astolfi <tastolfi@gmail.com>
2022-04-14 16:49:36 -0400

    fix script

--- script/publish-release.sh
+++ script/publish-release.sh
@@ -13,12 +13,12 @@ working_tree_is_clean || {
 active_version=$("${script_dir}/get-version.sh")
 release_version=$("${script_dir}/get-release.sh")
 
-if [ "$active_version" != "$release_version" ]; then
+if [[ "$active_version" != "$release_version" ]]; then
     echo "Error: active version (${active_version}) does not match the release version (${release_version}); did you forget to run ${script_dir}/tag-release.sh?" >&2
     exit 1
 fi
 
-if [ "$BATT_CONAN_UPLOAD_USER" == "" ]; then
+if [[ "${BATT_CONAN_UPLOAD_USER}" == "" ]]; then
     echo "Error: env var `BATT_CONAN_UPLOAD_USER` not specified!" >&2
     exit 1
 fi
