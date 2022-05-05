8a4a7fcf5bdb56239fd7354c781a711a57010746
Anthony Astolfi <tastolfi@gmail.com>
2022-05-05 17:02:39 -0400

    Add parallel algorithms: copy and merge.

---
+++ src/batteries/constants.hpp
@@ -0,0 +1,34 @@
+//######=###=##=#=#=#=#=#==#==#====#+==#+==============+==+==+==+=+==+=+=+=+=+=+=+
+// Copyright 2022 Anthony Paul Astolfi
+//
+#pragma once
+#ifndef BATTERIES_CONSTANTS_HPP
+#define BATTERIES_CONSTANTS_HPP
+
+#include <batteries/int_types.hpp>
+
+namespace batt {
+
+namespace constants {
+
+constexpr u64 kKB = 1000ull;
+constexpr u64 kMB = 1000ull * kKB;
+constexpr u64 kGB = 1000ull * kMB;
+constexpr u64 kTB = 1000ull * kGB;
+constexpr u64 kPB = 1000ull * kTB;
+constexpr u64 kEB = 1000ull * kPB;
+
+constexpr u64 kKiB = 1024ull;
+constexpr u64 kMiB = 1024ull * kKiB;
+constexpr u64 kGiB = 1024ull * kMiB;
+constexpr u64 kTiB = 1024ull * kGiB;
+constexpr u64 kPiB = 1024ull * kTiB;
+constexpr u64 kEiB = 1024ull * kPiB;
+
+}  // namespace constants
+
+using namespace constants;
+
+}  // namespace batt
+
+#endif  // BATTERIES_CONSTANTS_HPP
