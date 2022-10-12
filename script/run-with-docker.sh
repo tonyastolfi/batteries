3614cd126242d7cfdd5f92b287d43ff34615daee
Anthony Astolfi <tastolfi@gmail.com>
2022-10-12 14:02:39 -0400

    Fix run-with-docker script.

--- script/run-with-docker.sh
+++ script/run-with-docker.sh
@@ -3,10 +3,18 @@
 # Copyright (C) 2022 Anthony Paul Astolfi
 #
 
+# Figure out if the current shell is a TTY.
+#
+if [ -t 0 ]; then
+    DOCKER_FLAGS_INTERACTIVE=-it
+else
+    DOCKER_FLAGS_INTERACTIVE=
+fi
+
 # Run the passed arguments as a shell command in a fresh docker
 # container based on our CI image.
 #
-docker run -it \
+docker run ${DOCKER_FLAGS_INTERACTIVE} \
        -v /etc/passwd:/etc/passwd \
        -v /etc/group:/etc/group \
        --user $(id -u):$(id -g) \
@@ -14,5 +22,5 @@ docker run -it \
        -v "$(pwd)":"$(pwd)" \
        -v "$HOME/.conan":"$HOME/.conan" \
        -w "$(pwd)" \
-       registry.gitlab.com/tonyastolfi/batteries${BATT_DOCKER_IMAGE_EXT}:latest \
+       registry.gitlab.com/batteriescpp/batteries:latest \
        "$@"
