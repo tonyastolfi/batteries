e0a9171124c0a50d3c1885a24480fcb8d0ffc84e
Anthony Astolfi <tastolfi@gmail.com>
2024-06-10 10:16:06 -0400

    Add --update option to conan-lock-merge.sh.

--- conan-lock-merge.sh
+++ conan-lock-merge.sh
@@ -28,10 +28,18 @@ mkdir -p "${build_dir}"
 touch "${log_file}"
 echo "Generating conan.lock for: ${settings}  (writing output to ${log_file})"
 
+# If UPDATE=1 env var is set, then add the --update flag (check Conan remotes
+# for updated versions).  Default=1 (set UPDATE=0 to disable).
+#
+conan_lock_create_flags=
+if [ "${UPDATE:-1}" == "1" ]; then
+    conan_lock_create_flags=--update
+fi
+
 # Create lock file and do the merge, sending output to the log file.
 #
 (
-    conan lock create "${project_dir}" --lockfile="" --lockfile-out='tmp.conan.lock' ${settings}
+    conan lock create "${project_dir}" ${conan_lock_create_flags} --lockfile="" --lockfile-out='tmp.conan.lock' ${settings}
 
     if [ -e "${project_dir}/conan.lock" ]; then
         conan lock merge --lockfile="${project_dir}/conan.lock" --lockfile='tmp.conan.lock' --lockfile-out="${project_dir}/conan.lock"
