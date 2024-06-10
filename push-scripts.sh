e51cf70723a73c337f646269eb8f49ce7d4a40b0
Anthony Astolfi <tastolfi@gmail.com>
2024-06-10 09:52:46 -0400

    Add FORCE option to push-scripts.sh.

--- push-scripts.sh
+++ push-scripts.sh
@@ -8,6 +8,11 @@ if [ "${DEBUG:-}" == "1" ]; then
 fi
 
 script_dir=$(cd "$(dirname $0)" && pwd)
+extra_args=
+
+if [ "${FORCE:-}" == "1" ]; then
+    extra_args=-f
+fi
 
 cd "${script_dir}"
-git remote | xargs -n 1 -I {} git push {} script
+git remote | xargs -n 1 -I {} git push ${extra_args} {} script
