cb9949249d1a9d0a4c001222d6ef652d2c26a533
Anthony Astolfi <tastolfi@gmail.com>
2024-09-26 17:49:42 -0400

    Test runner script.

---
+++ run-tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+set -Eeuo pipefail
+
+script_dir="$(cd $(dirname $0) && pwd)"
+source "${script_dir}/common.sh"
+
+require_env_var BUILD_TYPE
+require_env_var project_dir
+
+build_dir="${project_dir}/build/${BUILD_TYPE}"
+
+mkdir -p "${build_dir}"
+cd "${build_dir}"
+build_dir="$(realpath)"
+
+function run_test() {
+    test_exe="$1"
+    
+    if [ "${GTEST_FILTER:-}" == "" ]; then
+	echo -e "\n\nRunning DEATH tests ==============================================\n"
+	GTEST_OUTPUT="xml:${build_dir}/death-test-results.xml" GTEST_FILTER='*Death*' "${test_exe}"
+        
+	echo -e "\n\nRunning non-DEATH tests ==========================================\n"
+        GTEST_OUTPUT="xml:${build_dir}/test-results.xml" GTEST_FILTER='*-*Death*' "${test_exe}"
+        
+    else
+	GTEST_OUTPUT="xml:${build_dir}/test-results.xml" "${test_exe}"
+    fi
+    
+}
+
+for name in $(find "${project_dir}/build/${BUILD_TYPE}" -type f -perm +0111 -name '*Test');
+do
+    run_test "${name}"
+done
