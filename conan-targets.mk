2ff77642dd2f11613b9e800008165c1cf5bafbf1
Anthony Astolfi <tastolfi@gmail.com>
2025-09-19 15:57:17 -0400

    build editable packages by default.

--- conan-targets.mk
+++ conan-targets.mk
@@ -72,7 +72,7 @@ endif
 #----- --- -- -  -  -   -
 
 CONAN_INSTALL_SH := $(SCRIPT_DIR)/conan-install.sh
-CONAN_INSTALL    := $(CONAN_ENV) $(MUTEX) $(CONAN_INSTALL_SH) $(CONAN_CONFIG_FLAGS) --build=missing $(OPTIONS)
+CONAN_INSTALL    := $(CONAN_ENV) $(MUTEX) $(CONAN_INSTALL_SH) $(CONAN_CONFIG_FLAGS) --build=missing --build=editable $(OPTIONS)
 CONAN_BUILD      := $(CONAN_ENV) conan build      $(CONAN_CONFIG_FLAGS) $(OPTIONS) -c tools.build:skip_test=True
 CONAN_EXPORT_PKG := $(CONAN_ENV) conan export-pkg $(CONAN_CONFIG_FLAGS) $(OPTIONS)
 CONAN_CREATE     := $(CONAN_ENV) conan create     $(CONAN_CONFIG_FLAGS) $(OPTIONS)
